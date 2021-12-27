719180c2622159fb26a8485bc61381859b67c2dd
NathanBENAIS <96385008+NathanBENAIS@users.noreply.github.com>
2021-12-27 19:53:27 +0100

    add

---
+++ src/components/Chart.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import {Bar, Line, Pie,Doughnut } from 'react-chartjs-2';
+
+export default class Chart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'City'
+  }
+
+  render(){
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+<br/>
+        <Line
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+         <Doughnut
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+
+
